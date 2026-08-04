1f7396f370348a67c3276b9b4d5beccd24647a02
plsticwtrbtl <pbbpkbdzr7@privaterelay.appleid.com>
2026-08-03 17:02:24 -0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+/* burger javascript */
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
